cfd191da4c4437b62e6d208cb48e4e4cf7d47b3f
stmav3069 <st3069@srmist.edu.in>
2026-02-17 15:14:27 +0530

    UC2: Render OOPS banner using print statements

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,6 +1,14 @@
 public class OOPSBannerApp {
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("This is UC1 feature branch");
+
+        System.out.println("  *****  " + "  *****  " + "  ****** " + "  ****** ");
+        System.out.println(" *     * " + " *     * " + " *     * " + " *     * ");
+        System.out.println(" *     * " + " *     * " + " *     * " + " *       ");
+        System.out.println(" *     * " + " *     * " + "  ****** " + "  *****  ");
+        System.out.println(" *     * " + " *     * " + " *       " + "       * ");
+        System.out.println(" *     * " + " *     * " + " *       " + " *     * ");
+        System.out.println("  *****  " + "  *****  " + " *       " + "  *****  ");
+
     }
 }
