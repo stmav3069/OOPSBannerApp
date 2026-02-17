ed5825a1ce8998cd021d0542abf4cb85a8ae12b1
stmav3069 <st3069@srmist.edu.in>
2026-02-17 14:46:11 +0530

    UC1: print Hello World message

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,5 +1,6 @@
 public class OOPSBannerApp {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("This is UC1 feature branch");
     }
 }
