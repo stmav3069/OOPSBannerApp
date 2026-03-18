26a89f5cbd78c222bb567438a986e05b96fd04eb
stmav3069 <st3069@srmist.edu.in>
2026-03-18 13:21:19 +0530

    UC8: Use HashMap for character patterns and render banner using function

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,54 +1,54 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class OOPSBannerApp {
 
-    public static String[] getOPattern() {
-        return new String[]{
-            " *** ",
-            "*   *",
-            "*   *",
-            "*   *",
-            "*   *",
-            "*   *",
-            " *** "
-        };
-    }
+    // Function to render banner
+    public static void renderBanner(String word, Map<Character, String[]> map) {
 
-    public static String[] getPPattern() {
-        return new String[]{
-            "*****",
-            "*   *",
-            "*   *",
-            "*****",
-            "*    ",
-            "*    ",
-            "*    "
-        };
-    }
+        int height = map.get(word.charAt(0)).length;
 
-    public static String[] getSPattern() {
-        return new String[]{
-            " ****",
-            "*    ",
-            "*    ",
-            " *** ",
-            "    *",
-            "    *",
-            "**** "
-        };
+        for (int i = 0; i < height; i++) {
+
+            StringBuilder line = new StringBuilder();
+
+            for (char c : word.toCharArray()) {
+                String[] pattern = map.get(c);
+                line.append(pattern[i]).append("  ");
+            }
+
+            System.out.println(line);
+        }
     }
 
     public static void main(String[] args) {
 
-        String[] oPattern = getOPattern();
-        String[] pPattern = getPPattern();
-        String[] sPattern = getSPattern();
+        Map<Character, String[]> patternMap = new HashMap<>();
 
-        for (int i = 0; i < oPattern.length; i++) {
-            System.out.println(
-                oPattern[i] + "   " +
-                oPattern[i] + "   " +
-                pPattern[i] + "   " +
-                sPattern[i]
-            );
-        }
+        patternMap.put('O', new String[]{
+                " *** ",
+                "*   *",
+                "*   *",
+                "*   *",
+                " *** "
+        });
+
+        patternMap.put('P', new String[]{
+                "**** ",
+                "*   *",
+                "**** ",
+                "*    ",
+                "*    "
+        });
+
+        patternMap.put('S', new String[]{
+                " ****",
+                "*    ",
+                " *** ",
+                "    *",
+                "**** "
+        });
+
+        renderBanner("OOPS", patternMap);
     }
 }
