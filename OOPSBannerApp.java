b50410286c49cc18aac2cd0d616dedddd53138e7
stmav3069 <st3069@srmist.edu.in>
2026-03-18 12:56:38 +0530

    UC6: Refactor OOPS banner using static methods

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,21 +1,55 @@
 public class OOPSBannerApp {
 
+    public static String[] getOPattern() {
+        return new String[]{
+            String.join(" ", " *** "),
+            String.join(" ", "*   *"),
+            String.join(" ", "*   *"),
+            String.join(" ", "*   *"),
+            String.join(" ", "*   *"),
+            String.join(" ", "*   *"),
+            String.join(" ", " *** ")
+        };
+    }
+
+    public static String[] getPPattern() {
+        return new String[]{
+            String.join(" ", "*****"),
+            String.join(" ", "*   *"),
+            String.join(" ", "*   *"),
+            String.join(" ", "*****"),
+            String.join(" ", "*    "),
+            String.join(" ", "*    "),
+            String.join(" ", "*    ")
+        };
+    }
+
+    public static String[] getSPattern() {
+        return new String[]{
+            String.join(" ", " ****"),
+            String.join(" ", "*    "),
+            String.join(" ", "*    "),
+            String.join(" ", " *** "),
+            String.join(" ", "    *"),
+            String.join(" ", "    *"),
+            String.join(" ", "**** ")
+        };
+    }
+
     public static void main(String[] args) {
 
-        String[] banner = {
+        String[] oPattern = getOPattern();
+        String[] pPattern = getPPattern();
+        String[] sPattern = getSPattern();
 
-            String.join("   ", " *** ", " *** ", " ***** ", " ***** "),
-            String.join("   ", "*   *", "*   *", "*    *", "  *     "),
-            String.join("   ", "*   *", "*   *", "*    *", "  *      "),
-            String.join("   ", "*   *", "*   *", " ***** ", " ***** "),
-            String.join("   ", "*   *", "*   *", "*      ", "      *"),
-            String.join("   ", "*   *", "*   *", "*      ", "      *"),
-            String.join("   ", " *** ", " *** ", "*      ", " ***** ")
-
-        };
-
-        for (String line : banner) {
-            System.out.println(line);
+        for (int i = 0; i < oPattern.length; i++) {
+            System.out.println(
+                oPattern[i] + "   " +
+                oPattern[i] + "   " +
+                pPattern[i] + "   " +
+                pPattern[i] + "   " +
+                sPattern[i]
+            );
         }
     }
 }
